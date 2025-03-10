d9873b308f19e27d95d4bf816129873c933f9afb
ginchanah <152182564+ginchanah@users.noreply.github.com>
2025-03-10 13:36:51 +0100

    3 artiesten opvragen en die filteren op populariteit

--- server.js
+++ server.js
@@ -197,12 +197,13 @@ async function getAccessToken() {
   });
 }
 
-// pitpull data van spotify opvragen
+// artist data van spotify opvragen
 async function fetchData() {
   try {
     const accessToken = await getAccessToken(); // Access token opvragen voordat de data opgevraagd wordt
     
-    const response = await fetch('https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg', {
+    //krijg een random array aan artiesten
+    const response = await fetch('https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx%2C57dN52uHvrHOxijzpIgu3E%2C1vCWHaC5f2uS3yhpwWbIA6', {
       headers: {
         Authorization: 'Bearer ' + accessToken
       }
@@ -211,13 +212,17 @@ async function fetchData() {
     const data = await response.json();
     console.log(data); // Log artist data
 
+    //sorteer de array en check naar artiesten die populariteit =< 10 hebben
+    let kleineArtiesten = data.artists.filter(artist => artist.popularity <= 70);
+    console.log("kleine Artiesten zijn: " + JSON.stringify(kleineArtiesten, null, 2))
+
   } catch (error) {
     console.error('Error fetching data:', error);
   }
+ 
 }
 
 // functie aanroepen
-//gaat nu niet werken vanwege dependencies
 fetchData();
 
 
---
+++ static/javascript/frontend.js
@@ -0,0 +1,1 @@
+https://api-website.com/countries?limit=195
