03272088c0c63809cf395aa2ec37b3a61a0419be
ginchanah <152182564+ginchanah@users.noreply.github.com>
2025-03-14 15:07:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -284,6 +284,7 @@ app.get("/token", (req, res) => {
 //     }
 // })
 
+
 //Als het goed is moet :artiest dan vervangen worden door iets van de api
 //Het klopt nog niet helemaal 100% en ik weet niet of dat aan de code ligt voor de session
 app.get("/favorieten/:artiest",async (req, res) => {
