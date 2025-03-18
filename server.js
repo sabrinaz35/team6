410052f7bda2180cec932f70dbda8cbeded3a449
sabrinaz35 <sj.zuurbier@gmail.com>
2025-03-18 16:20:12 +0100

    redirect de aanmeld account pagina naar de profiel toe + de juiste gegevens

--- server.js
+++ server.js
@@ -147,10 +147,20 @@ app.post('/add-account',upload.single('profielFoto'), async (req, res) => {
     //Even loggen om te checken of er een ID is aangemaakt
     console.log(`A document was inserted with the _id: ${toevoegen.insertedId}`);
 
-    //De controle hieronder werkt nog niet helemaal, ik wil namelijk dat hij teruggeeft of het gelukt is
-        if (doc){
-          //Groet de gebruiker, naam wordt overgenomen van de form, niet van de database
-          res.send(`Welkom, ${doc.name}! Account is succesvol aangemaakt.`)
+    //controleren of er daadwerkelijk een user is toegevoegd
+        if (toevoegen.insertedId){
+          //De nieuwe gebruiker vinden in de database
+          const newUser = await gebruiker.findOne({ emailadress: doc.emailadress });
+
+          //controleren of de gebruiker bestaat
+          if (newUser) {
+            console.log("Gebruiker is gevonden na het toevoegen");
+          }
+          //de gebruiker in een session zetten
+          req.session.user = newUser
+
+          //Na het aanmaken van de session meteen doorsturen naar profiel pagina met daarin de gegevens van de gebruiker
+          res.redirect("/profiel");
         } else {
           //Dit werkt helemaal nog niet :(
           res.send(`Oops er ging iets fout.`)
