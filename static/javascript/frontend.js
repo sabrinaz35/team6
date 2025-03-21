0ac8d17b907941cfe7a2d5ab4bc0d0bde3b33dd8
ginchanah <152182564+ginchanah@users.noreply.github.com>
2025-03-21 15:57:25 +0100

    genres randomizen en in input zetten

--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-//genres genereren uit spotify API
+//random genres genereren uit spotify API en deze naar de filter-genre pagina overzetten
 
 async function getGenres () {
   console.log("probeer genres op te halen")
@@ -127,16 +127,48 @@ async function getGenres () {
   
     console.log("Genres:", filteredGenres); 
 
-   
+    
+    const randomGenres = [];
+    let i = 0;
 
-   
+    while (i < 4) {
+      const random = Math.floor(Math.random() * filteredGenres.length);
+      
+      if (!randomGenres.includes(filteredGenres[random])) {
+        randomGenres.push(filteredGenres[random]);
+        i++;
+      }
+    }
 
+    console.log("Random genres:", randomGenres);
+   
+    //random genres aangeven op de genre filter pagina
+    document.getElementById('genre1Label').innerText = randomGenres[0];
+    document.getElementById('genre2Label').innerText = randomGenres[1];
+    document.getElementById('genre3Label').innerText = randomGenres[2];
+    document.getElementById('genre4Label').innerText = randomGenres[3];
+
+    //Gegevens ook meegeven aan de input voor de mongodb database
+    document.getElementById("genre1").value = randomGenres[0];
+    document.getElementById("genre2").value = randomGenres[1]; 
+    document.getElementById("genre3").value = randomGenres[2];
+    document.getElementById("genre4").value = randomGenres[3];
+
+    //uncheck de checkboxes bij het opnieuw laden
+    document.getElementById("genre1").checked = false;
+    document.getElementById("genre2").checked = false; 
+    document.getElementById("genre3").checked = false;
+    document.getElementById("genre4").checked = false;
 
   } catch {
     console.error('Error fetching genres:', error);
   }
 }
 
+//roep de getGenres functie aan om een eerste keuze aan genres te krijgen als je de pagina opent
+getGenres();
+
+//roep de functie opnieuw aan om ander genres te krijgen
 const genreLaden = document.querySelector("#genreLaden");
 genreLaden.addEventListener('click', getGenres);
 
