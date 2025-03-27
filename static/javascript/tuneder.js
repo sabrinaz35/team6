b9e50d31ce9d1af4febd6a628df041e2d65a7ad0
ginchanah <152182564+ginchanah@users.noreply.github.com>
2025-03-27 14:46:58 +0100

    javascript in pagina zetten waar het geen foutmelding geeft

--- static/javascript/tuneder.js
+++ static/javascript/tuneder.js
@@ -173,6 +173,30 @@ heartContainer.addEventListener('click', hartjeKlikken);
 
 
 
+//carousel 
+console.log("1")
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
 
 
 
