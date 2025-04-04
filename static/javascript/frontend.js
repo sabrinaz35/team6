43c67d77dd1189e05cad1da1dd361fa290d00a5e
David Jenniskens <david_jenniskens@hotmail.com>
2025-04-04 11:58:40 +0200

    html/css

--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -3,34 +3,15 @@
 console.log("Hallo wereld")
 
 
-// Hieronder komt de code voor het sorteren en filteren in opgeslagen artiesten
-// Wacht totdat de DOM volledig is geladen
-document.addEventListener('DOMContentLoaded', function() {
-  // Maak een nieuwe List.js instance voor de artiestenlijst
-  var opties = {
-      valueNames: ['naam'], // Dit is de naam waarop we gaan zoeken en sorteren
-      listClass: 'account-opgeslagen-artiest-grid' // Zorg ervoor dat je de juiste class gebruikt
+var options = {
+    // de classes van de h2's en p's (daarin gaat gezocht worden naar matches)
+    valueNames: [ 'name' ]
   };
-
-  var favorietenLijst = new List('favorieten', opties);
-
-  // Zoekfunctie
-  var zoekveld = document.querySelector('.search');
-  zoekveld.addEventListener('input', function() {
-      // Zoek op naam van de artiest
-      favorietenLijst.search(zoekveld.value);
-  });
-
-  // Sorteerfunctie
-  var sorteerknop = document.querySelector('.sort');
-  sorteerknop.addEventListener('click', function() {
-      // Sorteer de lijst op naam
-      favorietenLijst.sort('naam', { order: 'asc' });
-  });
-});
-
-
-
+  
+  
+  // Stap 3.
+  var charactersList = new List('theList', options);
+  
 // Like hartje index en opgeslagen pagina 
 //dit klopt nog niet helamaal oops
 let witHart = document.getElementsByClassName('likeHartje')
@@ -41,7 +22,4 @@ function veranderKleur (){
 
  witHart.classList.toggle ("roodHart");
 }
-
-
-
 
