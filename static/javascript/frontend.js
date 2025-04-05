8a05615fa1c45cbfb278a4e98cb79c9cd3ce7ceb
David Jenniskens <david_jenniskens@hotmail.com>
2025-04-05 16:32:59 +0200

    hamburgermenu-maken

--- static/javascript/frontend.js
+++ static/javascript/frontend.js
@@ -269,6 +269,25 @@ rangeSlider.addEventListener('input', function () {
     valueDisplay.innerHTML = rangeSlider.value;
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.getElementById('hamburger');
+  const mobileMenu = document.getElementById('mobileMenu');
+  const closeBtn = document.getElementById('closeBtn');
+  
+  console.log("Hamburger element:", hamburger);
+  console.log("Mobile menu element:", mobileMenu);
+  console.log("Close button element:", closeBtn);
+  // Toggle voor het openen/afsluiten van het mobiele menu
+  hamburger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+
+  // Sluit het menu wanneer je op het kruisje klikt
+  closeBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+  });
+});
+
 
 
 
